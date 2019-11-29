9ef8c171e217765d01b31aac9094dfe31d3dc050
AlymbekovvDastan <alymbek_uulu_d@iuca.kg>
2019-11-29 10:52:33 +0600

    The end

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView, BlogCreateView, \
+	BlogEditView, BlogDeleteView
 
 urlpatterns = [
 	path('', BlogListView.as_view(), name='home'),
-	path('<int:pk>/', BlogDetailView.as_view(), name='post_detail'), 
+	path('<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+	path('new/', BlogCreateView.as_view(), name='post_new'),
+	path('<int:pk>/edit/', BlogEditView.as_view(), name='post_edit'),
+	path('<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
 ]
